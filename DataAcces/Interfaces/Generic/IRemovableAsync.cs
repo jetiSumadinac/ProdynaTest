3c729d4bf8282d0dbbbf0dcf672d578b82569805
Jeti Sumadinac <sodis96067@amail1.com>
2021-02-11 01:07:48 +0100

    IRemovableAsync interface

---
+++ DataAcces/Interfaces/Generic/IRemovableAsync.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace DataAcces.Interfaces.Generic
+{
+    /// <summary>
+    /// Enables deleting existing entity in the database to selected date
+    /// </summary>
+    /// <typeparam name="T1">Entity's indenifier. Mostly <see cref="int"/>, <see cref="System.Guid"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    public interface IRemovableAsync<T1, in T2>
+    {
+        Task<T1> DeleteAsync(T2 data);
+    }
+}
