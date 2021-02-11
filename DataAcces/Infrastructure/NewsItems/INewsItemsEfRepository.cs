e8f7da7d086c1215cecd9bda216426c73a58bf40
Jeti Sumadinac <sodis96067@amail1.com>
2021-02-11 02:26:47 +0100

    newsItemsEfRepository initialized

---
+++ DataAcces/Infrastructure/NewsItems/INewsItemsEfRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAcces.Interfaces.Generic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAcces.Infrastructure.NewsItems
+{
+    public interface INewsItemsEfRepository :
+        IInsertableAsync<int, DataModels.NewsItems>,
+        ISearchableAsync<DataModels.NewsItems>,
+        IEditableAsync<int, DataModels.NewsItems>,
+        IRemovableAsync<int, Guid>
+    {
+    }
+}
---
+++ DataAcces/Infrastructure/NewsItems/NewsItemsEfRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Infrastructure.NewsItems
+{
+    public class NewsItemsEfRepository : BaseRepository<DataModels.NewsItems>, INewsItemsEfRepository
+    {
+        
+        public NewsItemsEfRepository()
+        {
+
+        }
+        public Task<int> DeleteAsync(Guid data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> EditAsync(DataModels.NewsItems data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataModels.NewsItems> GetSingleAsync(Expression<Func<DataModels.NewsItems, bool>> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertAsync(DataModels.NewsItems data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IQueryable<DataModels.NewsItems> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
